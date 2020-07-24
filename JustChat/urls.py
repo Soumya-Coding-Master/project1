5a03892b7a79a739174c7afbbe410e7da2a6e060
Soumya Gupta <badqueen12345abc@gmail.com>
2020-07-24 23:30:54 +0530

    first commit

---
+++ JustChat/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('chat/', include('chat.urls', namespace='chat')),
+    path('admin/', admin.site.urls),
+]
